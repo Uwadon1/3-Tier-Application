ed6778bcabda1b163946360bf00aec69021ceb35
Uwadone Joshua <111013277+Uwadon1@users.noreply.github.com>
2025-04-20 18:58:43 +0100

    Add files via upload

---
+++ Application Codes/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.ctq2wu6w4u9t.us-west-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Abanumchuks123',
+    DB_DATABASE : 'webappdb'
+});
